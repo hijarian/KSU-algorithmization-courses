e6e1e0135f05a880c5518c66c7c704fa1366b81b
Saphronov Mark <hijarian@gmail.com>
2012-05-27 11:09:31 +0400

    Добавил нормальные юнит-тесты для первого упражнения. Добавил документацию для первого упражнения.

--- Course 1/Semester 1/Algorithmisation and Programming/Lesson 01 [1.1]/After/L01Code.pas
+++ Course 1/Semester 1/Algorithmisation and Programming/Lesson 01 [1.1]/After/L01Code.pas
@@ -15,6 +15,10 @@ uses
 
 function MyFunction(x: real) : real;
 begin
+   if (x < 0.1) then
+      raise EInvalidArgument.Create('Expected x in [0.1, 0.6]');
+   if (x > 0.6) then
+      raise EInvalidArgument.Create('Expected x in [0.1, 0.6]');
    MyFunction := abs(sin(power(10.5*x, 1/2))) / (power(x, 2/3) - 0.143) + 2*pi*x;
 end;
 
--- Course 1/Semester 1/Algorithmisation and Programming/Lesson 01 [1.1]/After/L01Tests.pas
+++ Course 1/Semester 1/Algorithmisation and Programming/Lesson 01 [1.1]/After/L01Tests.pas
@@ -8,23 +8,55 @@ uses
 type
    Lesson1Tests = class(TTestCase)
    published    
-      procedure TestZeroInput;
+      procedure TestValidInput;
+      procedure TestNegativeXShouldThrowError;
+      procedure TestMinimumAllowedX;
+      procedure TestMaximumAllowedX;
    end;
 
 procedure RegisterTests;
 
 implementation
+uses
+   math;
+
+const
+   precision = 0.0001;
 
 procedure RegisterTests;
 begin
    TestFramework.RegisterTest(Lesson1Tests.Suite);
 end;
 
-procedure Lesson1Tests.TestZeroInput;
+procedure Lesson1Tests.TestValidInput;
 begin
-   Check(MyFunction(0) = 0, 'Function should be 0 when x is 0');
-end; { Lesson1Tests }
+   Check(MyFunction(0.1) - 12.4244944 < precision, 'On the left edge function should be calculable');
+   Check(MyFunction(0.35) - 4.8590140 < precision, 'Function should be calculable in the middle of interval');
+   Check(MyFunction(0.6)  - 4.8087379 < precision, 'On the right edge function should be calculable');
+end; 
 
+procedure Lesson1Tests.TestNegativeXShouldThrowError;
+begin
+   StartExpectingException(EInvalidArgument);
+   MyFunction(-1);
+   StopExpectingException();
+end;
+
+procedure Lesson1Tests.TestMinimumAllowedX;
+begin
+   StartExpectingException(EInvalidArgument);
+   MyFunction(0.1 - precision);
+   StopExpectingException();
+end;   
+
+procedure Lesson1Tests.TestMaximumAllowedX;
+begin
+   StartExpectingException(EInvalidArgument);
+   MyFunction(0.6 + precision);
+   StopExpectingException();
+end;   
+
+                  
 initialization
 end.
 
