9c116d792138db80769d491b636f07d4b7b6beca
Saphronov Mark <hijarian@gmail.com>
2012-06-17 11:29:06 +0400

    Исправил название файла с заданием.

--- Course 1/Semester 1/Algorithmisation and Programming/Lesson 01/After/Lesson01.pas
+++ Course 1/Semester 1/Algorithmisation and Programming/Lesson 01/After/Lesson01.pas
@@ -1,10 +1,10 @@
 {
  КГУ, семестр 1, курс "Алгоритмизация и программирование", задание 1
- 
+
  Требуется получить от пользователя число из заданного интервала и вычислить
  сложное математическое выражение от этого числа.
 
- Полное задание в приложенном файле Lesson1.task.png
+ Полное задание в приложенном файле Task01.tex
 
  Автор: hijarian@gmail.com
  Лицензия: Public domain
@@ -18,15 +18,15 @@ uses
 var
    x: real;
    answer: real;
-   
+
 begin
    writeln('Enter 0.1 =< x <= 0.6 here:');
    repeat
       readln(x)
    until
       (x>=0.1) and (x<=0.6);
-   
+
    answer:= MyFunction(x);
-   
+
    writeln('Answer is ',answer:12:3);
 end.
