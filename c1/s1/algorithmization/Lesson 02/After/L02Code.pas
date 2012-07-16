3cd8601f5df9f3bd405feb4d9bcdc6bdb2b82fe1
Saphronov Mark <hijarian@gmail.com>
2012-07-16 13:48:55 +0400

    Added tested code to second lesson. One test fails badly because of large rounding error.

--- c1/s1/algorithmization/Lesson 02/After/L02Code.pas
+++ c1/s1/algorithmization/Lesson 02/After/L02Code.pas
@@ -8,21 +8,50 @@ interface
 
 function CalcSum(x: real; n: integer) : real;
 
+function Mymod(k: integer; x: real) : real;
+function Mysqre(k: integer) : real;
+function Myln(k: integer; x: real) : real;
+function MyIteration(k: integer; x: real) : real;
+
 implementation
 
 uses
-  Math;
+   Math;
+
+function Mymod(k: integer; x: real) : real;
+begin
+   result := sqr(abs(x - k));
+end;
+
+function Mysqre(k: integer) : real;
+begin
+   result := sqrt(exp(k - 1));
+end;
+
+function Myln(k: integer; x: real) : real;
+begin
+   result := ln ( 2 + power(x, k)  + power(x, (2*k+1)) );
+end;
+
+function MyIteration(k: integer; x: real) : real;
+begin
+   if (k = 0) and (x = -2) then
+      raise EInvalidArgument.Create('if k = 0 and x = -2 then the iteration will divide by zero, it''s not allowed!');
+
+   result := Mymod(k, x) * Mysqre(k) / Myln(k, x);
+end;
 
 function CalcSum(x: real; n: integer) : real;
 var
-  Sum: real;
-  i: integer;
+   Sum: real;
+   i: integer;
 begin
-  Sum := 0;
-  for i := 1 to n do begin
-    Sum := Sum + (sqr(abs(x-i)) * sqrt(exp(i-1)) / (ln(2+power(x,i)) + power(x, (2*i+1))));
-  end;
-  Sum := Sum*exp(sqrt(x/n));
+   Sum := 0;
+   for i := 1 to n do begin
+      Sum := Sum + MyIteration(i, x);
+   end;
+   Sum := Sum * exp(sqrt(x/n));
+   result := Sum;
 end;
 
 initialization
--- c1/s1/algorithmization/Lesson 02/After/L02Tests.pas
+++ c1/s1/algorithmization/Lesson 02/After/L02Tests.pas
@@ -9,19 +9,71 @@ uses
 type
    Lesson02Tests = class(TTestCase)
    published    
-      // ЗДЕСЬ ОБЪЯВЛЕНИЯ ТЕСТОВЫХ ПРОЦЕДУР
+      procedure TestMymodWithValidInput;
+      procedure TestMysqreWithValidInput;
+      procedure TestMylnWithValidInput;
+      procedure TestCannotDivideByZero; 
+      procedure TestIterationShouldRunWithAnyX;
    end;
 
 procedure RegisterTests;
 
 implementation
 
+uses
+   math, sysutils;
+
+const
+   precision = 0.0001;
+
 procedure RegisterTests;
 begin
    TestFramework.RegisterTest(Lesson02Tests.Suite);
 end;
 
-// ЗДЕСЬ ТЕСТОВЫЕ ПРОЦЕДУРЫ
+procedure Lesson02Tests.TestMymodWithValidInput;
+begin
+   Check(Mymod(2, 4) - 4 < precision);
+   Check(Mymod(4, 2) - 4 < precision);
+   Check(Mymod(-4, 2) - 36 < precision);
+   Check(Mymod(4, -2) - 36 < precision);
+   Check(Mymod(0, -3) - 9 < precision);
+   Check(Mymod(-3, 0) - 9 < precision);
+end;
+
+procedure Lesson02Tests.TestMysqreWithValidInput;
+begin
+   Check(Mysqre(1)  - 1.0000000 < precision);
+   Check(Mysqre(2)  - 1.6487212 < precision);
+   Check(Mysqre(0)  - 0.6065306 < precision);
+   Check(Mysqre(-1) - 0.3678794 < precision);
+end;
+
+procedure Lesson02Tests.TestMylnWithValidInput;
+begin
+   Check(Myln(0, 2)      - 1.609438 < precision);
+   Check(Myln(1, 2)      - 2.484906 < precision);
+   Check(Myln(34, -1)    - 0.693147 < precision);
+   Check(Myln(5, 2.1525) - 8.443364 < precision);
+   Check(Myln(-2, 3)     - 0.7646061 < precision);
+end;
+
+procedure Lesson02Tests.TestCannotDivideByZero;
+begin
+   StartExpectingException(EInvalidArgument);
+   MyIteration(0, -2);
+   StopExpectingException();
+end;
+
+procedure Lesson02Tests.TestIterationShouldRunWithAnyX;
+var temp: real;
+begin
+   Check(MyIteration(1, 1) - 0 < precision);
+   Check(MyIteration(1, 10)   - 11.705737 < precision);
+   Check(MyIteration(2, -1) - 21.407417 < precision);
+   temp := MyIteration(20, 0.5);
+   Check(temp - 7328937.5 < precision, 'Expected: 7328937.5, got:' + floattostr(temp));
+end;
 
 initialization
 end.
