7b73301e5f0381e8463e18deebd8695fe1bc48d5
Mark Safronov <hijarian@gmail.com>
2026-03-13 23:12:30 +0100

    чистый код ООП 02 без продакшен-обвеса

--- c1/s2/OOP_02/srcCore_02.cpp
+++ c1/s2/OOP_02/srcCore_02.cpp
@@ -1,38 +1,89 @@
+// For M_PI constant
+#define _USE_MATH_DEFINES
+
 #include <cstdio>
 #include <cmath>
-//#include <windows.h>
-//#include "ConvOem.h"
+
+constexpr auto MY_FAIL_READ_CYLINDERS_NUMBER = -1;
+constexpr auto MY_FAIL_READ_CYLINDERS_HEIGHT = -2;
+constexpr auto MY_FAIL_READ_FIRST_CYLINDER_RADIUS = -3;
+
+constexpr double METAL_SHEET_THICKNESS = 0.2;
+constexpr int METAL_SHEET_DENSITY = 5000;
+
+double getCylinderVolume(double radius, double height) {
+	return radius * radius * M_PI * height;
+}
+
+double calculateReservoirWallsVolume(double firstReservoirRadius, int i, double cylindersHeight) {
+	double innerRadius = firstReservoirRadius * sqrt((double)i);
+	double externalRadius = innerRadius + METAL_SHEET_THICKNESS;
+	double innerHeight = cylindersHeight - METAL_SHEET_THICKNESS * 2;
+	return getCylinderVolume(externalRadius, cylindersHeight) - getCylinderVolume(innerRadius, innerHeight);
+}
+
+double calculateTotalReservoirsVolume(int reservoirsNumber, double firstReservoirRadius, double cylindersHeight) {
+	double totalReservoirsVolume = 0.0;
+	for (int i = 1; i <= reservoirsNumber; ++i)
+	{
+		double currentReservoirVolume = calculateReservoirWallsVolume(firstReservoirRadius, i, cylindersHeight);
+		printf("Volume of walls of reservoir #%i: %12.2lf\n", i, currentReservoirVolume);
+		totalReservoirsVolume += currentReservoirVolume;
+	}
+	return totalReservoirsVolume;
+}
+
+int askForInteger(const char* prompt, const char* errorMessage, int errorExitCode) {
+	printf(prompt);
+	int value;
+	int entriesRead = scanf("%i", &value);
+	if (entriesRead < 1) {
+		printf(errorMessage);
+		exit(errorExitCode);
+	}
+	return value;
+}
+
+int askForReservoirsNumber() {
+	return askForInteger(
+		"Enter number of cylinders:\n",
+		"Error reading number of cylinders",
+		MY_FAIL_READ_CYLINDERS_NUMBER
+	);
+}
+
+int askForReservoirsHeight() {
+	return askForInteger(
+		"Enter height of cylinders (it equals for each):\n",
+		"Error reading height of cylinders",
+		MY_FAIL_READ_CYLINDERS_HEIGHT
+	);
+}
+
+int askForFirstReservoirRadius() {
+	return askForInteger(
+		"Enter inner radius of 1st cylinder: \n",
+		"Error reading inner radius of the 1st cylinder",
+		MY_FAIL_READ_FIRST_CYLINDER_RADIUS
+	);
+}
 
 int main()
 {
-	//CConvOem szBuf(100);
-	printf("Enter nmbr of cylinders: \n");
-	int nmb;
-	scanf("%i",&nmb);
-	printf("Enter height of cylinders (it equals for each):\n");
-	int h;
-	scanf("%i",&h);
-	printf("Enter inner radius of 1st cylinder: \n");
-	int r;
-	scanf("%i",&r);
-	const double thick=0.2;
-	const int dens=5000;
-	printf("Cylinder wall thickness equals %3.1f\n",thick);
-	printf("Metal density equals %i\n",dens);
-	double volCur, volSum=0.0;
-	const double pi=3.14159;
-	for (int i=1;i<=nmb;++i)
-	{
-		double rc;
-		rc=r*sqrt((double)i);
-		double extrad=rc+thick;
-		volCur=(pow(extrad,2)-pow(rc,2))*pi*h;
-		printf("Volume of cylinder #%i: %12.2lf\n",i,volCur);
-		volSum+=volCur;
-	}
-	double mass=volSum*dens;
-	printf("Summary volume of cylinders' walls: %12.2f\n",volSum);
-	printf("Summary mass of cylinders: %12.2f\n",mass);
+	int reservoirsNumber = askForReservoirsNumber();
+
+	int reservoirsHeight = askForReservoirsHeight();
+
+	int firstReservoirRadius = askForFirstReservoirRadius();
+
+	printf("Cylinder wall thickness equals %3.1f\n", METAL_SHEET_THICKNESS);
+	printf("Metal density equals %i\n", METAL_SHEET_DENSITY);
+
+	double totalReservoirsVolume = calculateTotalReservoirsVolume(reservoirsNumber, firstReservoirRadius, reservoirsHeight);
+	printf("Summary volume of reservoirs' walls: %12.2f\n", totalReservoirsVolume);
+
+	double totalMass = totalReservoirsVolume * METAL_SHEET_DENSITY;
+	printf("Summary mass of reservoirs' walls: %12.2f\n", totalMass);
 
 	return 0;
 }
